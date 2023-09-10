f0a89d00d9f489e20f49bfc67ed3b6bfea9e5231
jiwon317 <chlghqlt12@naver.com>
2023-09-10 14:49:17 +0900

    initial commit

---
+++ src/hello_world/class1.java
@@ -0,0 +1,11 @@
+package hello_world;
+
+public class class1 {
+	public static void main(String[] args) {
+		
+		long k = 5000;
+		System.out.println(k);
+		
+	}
+
+}
---
+++ src/hello_world/main.java
@@ -0,0 +1,70 @@
+package hello_world;
+
+public class main {
+//	System.out.println("")
+	public static void main (String args[]) {
+		int i;
+		i = 01031242117;
+		System.out.println(i);
+		i = 123;
+		System.out.println(i);
+		
+		String str;
+		str = "01031242117";
+		int a = 29;
+		System.out.println("hello world");
+		System.out.println(i);
+		System.out.println(str);
+		System.out.println(a);
+		
+		final double PI = 3.141592;
+		final int AGE = 29;
+//		AGE = 35;
+		boolean flag = false;
+		char c = 'a';
+		String cc = "aa";
+		boolean choi = true;
+		char kang = 'S';
+		byte nasus = 100;
+		short starbucks = 3;
+		int thinkpad = 10;
+		long mymind = 100000;
+//		float f = 0.100;
+		double k = 0.48;
+		int i1 = 1;
+		int i2 = 2;
+		int i3 = i1+i2;
+		System.out.println(i3);
+		int i4 = 5 % 4;
+		
+		int sample = 3;
+		if (sample % 2 == 0) {
+			System.out.println("2의 배수입니다.");
+		} else {
+			System.out.println("2의 배수가 아닙니다.");
+		}
+		
+		int q = 5/3;
+		int w = 5%3;
+		
+		System.out.println(q);
+		System.out.println(w);
+		
+		//증감연산자
+		
+		int plus = 1;
+		System.out.println("----------------------");
+		System.out.println(++plus);
+		System.out.println(plus++);
+		System.out.println(plus);
+		
+		String Choi = "genius";
+		System.out.println(Choi);
+		
+		
+		
+				
+		
+	}
+
+}
