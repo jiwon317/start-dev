87c28568abbc18411ab9dc714368a061d6c945ff
jiwon317 <chlghqlt12@naver.com>
2023-09-14 15:02:23 +0900

    commit message

---
+++ src/hello_world/class_2.java
@@ -0,0 +1,18 @@
+package hello_world;
+
+public class class_2 {
+ public static void main(String[]args) {
+	String myfirstname = "Choi";
+	System.out.println(myfirstname);
+	long mybirthday = 317;
+	System.out.println(mybirthday);
+	String nickname = "ronaldo";
+	System.out.println(nickname);
+	
+	
+	
+	
+	
+			
+ }
+}
