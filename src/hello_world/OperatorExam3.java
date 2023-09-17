dddafe6b823ad3983b60fd9aac9ccb4289e2e559
jiwon317 <chlghqlt12@naver.com>
2023-09-17 22:35:24 +0900

    commit message

---
+++ src/hello_world/OperatorExam3.java
@@ -0,0 +1,21 @@
+package hello_world;
+
+public class OperatorExam3 {
+
+	
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		int c = 15;
+		
+		System.out.println(a - b * c);
+		System.out.println((a - b) * c);
+		System.out.println(a > 5 && b > 5);
+		System.out.println(a > 5 || b > 5);
+		
+//		++a -> a = a + 1
+		System.out.println(a++ - 5);
+		System.out.println(a);
+		
+	}
+}
