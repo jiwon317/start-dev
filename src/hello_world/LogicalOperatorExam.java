5e12f0cc3b49a81ebffa3b80d0e0284a96ade5d8
jiwon317 <chlghqlt12@naver.com>
2023-09-18 23:14:10 +0900

    commit message

---
+++ src/hello_world/LogicalOperatorExam.java
@@ -0,0 +1,13 @@
+package hello_world;
+
+public class LogicalOperatorExam {
+	public static void main(String[] args) {
+		int score = 65;
+		if(score >= 70 && score <= 100) {
+			System.out.println("성공");
+		}else {
+			System.out.println("실패");
+		}
+		
+	}
+}
