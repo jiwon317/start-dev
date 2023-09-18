eb729eddacb8c44e05b12b19260941e7a2962690
jiwon317 <chlghqlt12@naver.com>
2023-09-19 00:12:23 +0900

    commit message

--- src/hello_world/TernaryExam.java
+++ src/hello_world/TernaryExam.java
@@ -1,9 +1,13 @@
 package hello_world;
 
-public class TernaryExam {
-	
+import java.util.Calendar;
+public class TernaryExam{
 	public static void main(String[] args) {
-		int b1 = (5 > 4) ? 50 : 40;
+		int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+		String ampm;
+		ampm = (hour < 12) ? "오전" : "오후";
+		System.out.println("지금 시간은 "+hour+"시이고, "+ampm+"입니다.");
+		
 		
 	}
 }
