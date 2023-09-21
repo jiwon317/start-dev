c5a8a39c048da4fdc2d8ef0ee8764da3818ee5f6
jiwon317 <chlghqlt12@naver.com>
2023-09-21 23:23:51 +0900

    commit message

--- src/hello_world/ArrayExam.java
+++ src/hello_world/ArrayExam.java
@@ -1,12 +1,26 @@
 package hello_world;
 
 public class ArrayExam {
-	public static void main(String[] args) {
-		int[] array2 = new int[]{1,2,3,4};
-		int[] array3 = {1,2,3,4};
+	public int[] makeArray() {
+		int[] array = new int[]{1,2,3,4,5};
 		
-		int value = array3[0];
-		System.out.println(value);
-		
+		return array;
+	}
+	
+	public static void main(String[]args) {
+		ArrayExam exam = new ArrayExam();
+		int [] array = exam.makeArray();
+		if(array.length==5 &&
+			array[0] ==1 &&
+			array[1] ==2 &&
+			array[2] ==3 &&
+			array[3] ==4 &&
+			array[4] ==5) {
+			System.out.println("정답입니다.");
+		}
+		else {
+			System.out.println("틀렸습니다.");
+			
+		}
 	}
 }
