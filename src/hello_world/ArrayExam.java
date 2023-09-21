0816eb5552204ec107a88dbcd58f203a67cf7ae0
jiwon317 <chlghqlt12@naver.com>
2023-09-21 23:18:38 +0900

    commit message

--- src/hello_world/ArrayExam.java
+++ src/hello_world/ArrayExam.java
@@ -2,12 +2,11 @@ package hello_world;
 
 public class ArrayExam {
 	public static void main(String[] args) {
-		int[] array1 = new int[100];
+		int[] array2 = new int[]{1,2,3,4};
+		int[] array3 = {1,2,3,4};
 		
-		
-		array1[0] = 50; 
-		array1[10] = 100;
+		int value = array3[0];
+		System.out.println(value);
 		
 	}
-
 }
