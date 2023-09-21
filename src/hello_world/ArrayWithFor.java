924c41ef14c0f8386e8e4ddebf8c98a3bef99f1c
jiwon317 <chlghqlt12@naver.com>
2023-09-21 23:35:22 +0900

    commit message

---
+++ src/hello_world/ArrayWithFor.java
@@ -0,0 +1,18 @@
+package hello_world;
+
+public class ArrayWithFor {
+	public static void main(String[] args) {
+		int[] iarray = new int[100];
+//		for(int i = 0; 조건식; 증감식)
+//		for(int i = 0; i < 100; 증감식)
+//		for(int i = 0; i < iarray.length; 증감식)
+		for(int i = 0; i < iarray.length; i++) {
+			iarray[i] = i + 1;
+		}
+		int sum = 0;
+		for(int i = 0; i < iarray.length; i++) {
+			sum = sum + iarray[i];
+			}
+			System.out.println(sum);
+	}
+}
