82bd64d8f734cac7c637098777e65ea71cfa2722
jiwon317 <chlghqlt12@naver.com>
2023-09-24 23:52:33 +0900

    star testing

---
+++ src/hello_world/startest0924.java
@@ -0,0 +1,12 @@
+package hello_world;
+
+public class startest0924 {
+	public static void main (String[] args) {
+		for (int i = 0; i < 2; i++) {
+			for (int j = 1; j < 2 * i + 1; j++) {
+				System.out.println("!");
+			}
+		}
+		System.out.println();	
+	} 
+}
