aca72001483ab5119aa8d1c3fda58a46f6fbc2d4
jiwon317 <chlghqlt12@naver.com>
2023-09-25 00:29:42 +0900

    startesting2

--- src/hello_world/startest0924.java
+++ src/hello_world/startest0924.java
@@ -2,11 +2,17 @@ package hello_world;
 
 public class startest0924 {
 	public static void main (String[] args) {
-		for (int i = 0; i < 2; i++) {
-			for (int j = 1; j < 2 * i + 1; j++) {
-				System.out.println("!");
+		for (int i = 1 ; i < 5; i++) {
+			if ( i % 2 == 0) {
+				System.out.print("!!");
+				System.out.println("!!");
+				}
+			else if(i % 2 == 1) {
+					System.out.println("!");
+				}
 			}
+	
 		}
-		System.out.println();	
-	} 
-}
+			
+} 
+
